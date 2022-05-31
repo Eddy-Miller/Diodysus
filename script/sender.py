a588da2b8e154e80488cbd8efff0c949f948408f
Lorenzo Colombi <lorenzo.colombi@edu.unife.it>
2022-05-31 14:50:39 +0200

    Creato sender.py (solo modalità ethernet)

---
+++ script/sender.py
@@ -0,0 +1,62 @@
+#import
+import random, socket, os , sys, time
+
+#global variable and costants
+MODE_LIST = ["ethernet", "serial", "infrared"]
+
+msg = { "sensor_name": "sensor_name_placeholder", "sensor_value": "sensor_value_placeholder" }
+
+#comunication functions
+def ethernet_mode(address,port):
+    #UDP socket setup
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    while True:
+        #reading data from the sensors
+        msg = random_sensor_value()
+        #send message
+        msg = str(msg)
+        sock.sendto(msg.encode(), (address, port) )
+        print("Sent: {}, to {}:{}".format(msg, address, port))
+        #wait for a while
+        time.sleep(1)
+
+def serial_mode():
+    pass
+
+def infrared_mode():
+    pass
+
+#utility functions
+def random_sensor_value():
+    msg["sensor_value"] = random.randint(0, 100)
+    msg["sensor_name"] ="random_value"
+
+    return msg
+
+def main():
+    #check number of arguments equal to 2
+    if len(sys.argv) != 2:
+        print("Usage: python sender.py <mode>\nMode can be: ethernet, serial or infrared")
+        sys.exit(1)
+    
+    #check if mode is valid
+    if sys.argv[1] not in MODE_LIST:
+        print("Invalid mode: {}\nMode can be: ethernet, serial or infrared".format(sys.argv[1]))
+        sys.exit(1)
+    
+    mode = sys.argv[1]
+    print("Hello World - I'm the sender in mode: " + mode)
+
+    #execute the current mode
+    if(mode == "ethernet"):
+        ethernet_mode("localhost", 5555)
+    elif(mode == "serial"):
+        serial_mode()
+    elif(mode == "infrared"):
+        infrared_mode()
+    
+
+
+if __name__ == "__main__":
+    main()
